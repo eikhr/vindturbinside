e3da499908053d1e56061d0fd9759f54ca49f1d9
Eik <eik.rogeberg@gmail.com>
2018-08-27 20:02:57 +0200

    la til nødvendige filer for Trubin På Rad

--- ruter/spill/turbinPaaRad.js
+++ ruter/spill/turbinPaaRad.js
@@ -11,9 +11,9 @@ ruter.get('/', asyncMiddleware(async (req, res, next) => {
 
 	
 	req.hbsdata.headLenker += `	<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-							    <link rel="stylesheet" href="./App.css">
-							    <link rel="stylesheet" href="./index.css">
-							    <link rel="stylesheet" href="./Game.css">`;
+							    <link rel="stylesheet" href="/CSS/spill/TPR/App.css">
+							    <link rel="stylesheet" href="/CSS/spill/TPR/index.css">
+							    <link rel="stylesheet" href="/CSS/spill/TPR/Game.css">`;
 	req.hbsdata.tittel = 'Turbin På Rad!';
 
 	lagSide(req, res, 'spill/turbinPaaRad', req.hbsdata, next);
