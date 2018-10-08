3b37177f41afa2a62807743a9a218ee94dd4a1fc
Eik <eik.rogeberg@gmail.com>
2018-10-08 16:08:31 +0200

    små endringer

--- ruter/spill/eventyrspill.js
+++ ruter/spill/eventyrspill.js
@@ -9,7 +9,7 @@ const ruter = express.Router();
 ruter.get('/', asyncMiddleware(async (req, res, next) => {
 	req.aktivSide = 'spill';
 
-	req.hbsdata.headLenker += ``;
+	req.hbsdata.headLenker += `<style>.innediv {background-color: rgba(50,50,60,0.9);}</style>`;
 	req.hbsdata.tittel = 'Eventyrspill - Vindturbinismen';
 	req.hbsdata.js += '<script src="/JS/spill/eventyrspill.js"></script>';
 
