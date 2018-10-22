b9d3cdfac401bc77790cdce9e84b0e6c88726c87
Eik <eik.rogeberg@gmail.com>
2018-10-22 20:59:16 +0200

    removed unused config file and removed old code from dbTilkobling.js

--- serverfiler/database/dbTilkobling.js
+++ serverfiler/database/dbTilkobling.js
@@ -1,8 +1,6 @@
 const mysql = require('mysql');
 const mysqlssh = require('mysql-ssh');
 
-const config = require('../config.js');
-
 
 /*
 module.exports = async () => {
@@ -28,47 +26,26 @@ module.exports = async () => {
 	return con;
 };
 /*/
-if(config.dev) {
-	module.exports = async function() {
-		console.log('Starter tilkobling til databasen');
-		try {
-			let con = await mysqlssh.connect(
-				{
-					host: 'ssh.phx.nearlyfreespeech.net',
-					user: 'vindturbin_turbin',
-					port: 22,
-					password: 'miwoofrioxoe'
-				},
-				{
-					host: 'vindturbin.db',
-					user: 'script',
-					password: '##ReM%lp4B5V',
-					database: 'turbin'
-				}
-			);
-			console.log('Koblet til databasen!');
-			return con;
-		} catch (err) {
-			console.log('Kunne ikke koble til databasen: ' + err);
-		}
-	};
-} else {
-	module.exports = async function() {
-		let con = mysql.createConnection({
-			host: 'vindturbin.db',
-			user: 'script',
-			password: '##ReM%lp4B5V',
-			database: 'turbin'
-		});
-
-		con.connect(function(err) {
-			if (err) {
-				console.log('Kunne ikke koble til databasen: ' + err);
-			} else {
-				console.log('Koblet til databasen!');
-				return con;
+module.exports = async function() {
+	console.log('Starter tilkobling til databasen');
+	try {
+		let con = await mysqlssh.connect(
+			{
+				host: 'ssh.phx.nearlyfreespeech.net',
+				user: 'vindturbin_turbin',
+				port: 22,
+				password: 'miwoofrioxoe'
+			},
+			{
+				host: 'vindturbin.db',
+				user: 'script',
+				password: '##ReM%lp4B5V',
+				database: 'turbin'
 			}
-		});
-	};
-}
-//*/
+		);
+		console.log('Koblet til databasen!');
+		return con;
+	} catch (err) {
+		console.log('Kunne ikke koble til databasen: ' + err);
+	}
+};
